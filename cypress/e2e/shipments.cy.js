f6a68aa53661c97b88bc2e43c4b03d4f3a2cbc8d
Amauri Lima <amauri.plimaj@gmail.com>
2024-09-18 22:17:45 -0300

    adiciona quais casos de teste faremos

--- cypress/e2e/shipments.cy.js
+++ cypress/e2e/shipments.cy.js
@@ -19,12 +19,27 @@ describe('shipments', () => {
     // Assert that shipment has been shipped
     cy.get('body').should('contain', 'Shipment has been successfully shipped.');
   });
-  it('test case 2', () => {
+  it('Testar se caso a lista de shipment vier vazia, o warning aparece', () => {
     // Implement your test case 2 code here
   });
-  it('test case 3', () => {
+  it('Testar se o botão clear filter está limpando todos os filtros', () => {
     // Implement your test case 3 code here
   });
-
-  // Implement the remaining test cases in a similar manner
+  it('Testar se o botão de order está mandando pra pagina correta admin/orders/{valor do botão}', () => {
+    // Implement your test case 3 code here
+  });
+  it('Verificar se o state se mantém igual tanto na listagem quanto na pagina de detalhes', () => {
+    // Implement your test case 3 code here
+  });
+  it('Testar se enviando uma encomenda com codigo de tracking, o codigo se mantém na pagina de order atraves do botão show', () => {
+    // Implement your test case 3 code here
+  });
+  it('Testar se botão de previous e next funcionam corretamente', () => {
+    // Implement your test case 3 code here
+  });
+  it('Testar se a box filters esconde e mostra o conteudo corretamete', () => {
+    // Implement your test case 3 code here
+  });
+  it('Verificar se os filtros de ordenação funcionam corretamente', () => {});
+  it('Verificar as linhas com status Shipped não tem o botão Ship, e se a linha com status READY tem', () => {});
 });
